bcf3be3159edb8cd84ad81a3f4f8997a1e056212
li.xiang <li.xiang@shopee.com>
2022-07-01 10:24:41 +0800

    feat: add inorder binary tree

--- binaryTree/bst-iterate.js
+++ binaryTree/bst-iterate.js
@@ -1,9 +1,11 @@
+// 二叉树迭代遍历
+
 class Node {
-	constructor(val) {
-		this.key = val;
-		this.left = null;
-		this.right = null;
-	}
+  constructor(val) {
+    this.key = val;
+    this.left = null;
+    this.right = null;
+  }
 }
 /**
  * Definition for a binary tree node.
@@ -20,18 +22,21 @@ class Node {
 
 // 中序遍历 非递归
 function inorderBinaryTree(root) {
-    let cur = root
-    let stack = []
-    let res = []
-
-    while (cur !== null || stack.length) {
-        while (cur !== null) {
-            stack.unshift(cur)
-            cur = cur.left
-        }
-        cur = stack.shift()
-        res.push(cur.val)
-        cur = cur.right
+  /**
+   * 使用root作为current指针
+   *
+   */
+  const res = [];
+  const stk = [];
+  while (root || stk.length) {
+    while (root) {
+      stk.push(root);
+      root = root.left; // 不断将当前节点的左子树放进栈中
     }
-    return res
+    // root为null说明最后放进栈中的节点是：1. 一个叶子节点 或 2.没有左子树的父节点。应该被弹出栈，将val放入res
+    root = stk.pop();
+    res.push(root.val);
+    root = root.right; // 此时当前节点的右子树对应两种情况: 1. right为null 2.right存在，为下一个应放入栈中的节点
+  }
+  return res;
 }
