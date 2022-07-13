f0250b3d9a394e7e18ddd3310a386152bd5ab905
xiangli <healy@192.168.0.105>
2022-07-14 00:18:13 +0800

    feat: add level walk tree

--- binaryTree/level-walk.js
+++ binaryTree/level-walk.js
@@ -22,3 +22,29 @@ function walkCurLevel(root, i) {
         walkCurLevel(root.right, i - 1)
     }
 }
+
+// 层序遍历迭代实现
+/**
+ *  使用一个队列来存放遍历的节点，通过判断队列长度，即为该层中节点个数 
+ *  从队列中取出元素时，不是一次取一个，而是取出一层中的元素
+ */
+var levelOrder = function(root) {
+    let res = []
+    if (!root) {
+        return res
+    }
+    let queue = []
+    queue.push(root)
+    while (queue.length) {
+        let resSize = res.length;
+        res[resSize] = []
+        let size = queue.length // size即为当前层的节点个数
+        while (size--) {
+            let cur = queue.shift()
+            res[resSize].push(cur.val)
+            cur.left && queue.push(cur.left)
+            cur.right && queue.push(cur.right)
+        }
+    }
+    return res;
+};
